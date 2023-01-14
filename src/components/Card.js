d792aca158a2da009ff9d792ed49707126ddd41d
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-01-14 14:14:30 +0100

    First approach

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div>
+        <h1>Card</h1>
+        <h2>Card h2</h2>
+      </div>
+      <div>asdasd</div>
+    </>
+  );
+};
+
+export default Card;
