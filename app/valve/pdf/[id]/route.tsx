fa24cd8905a6cf65539ea060e0e8a86d10aa74c2
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-11-25 15:48:31 +0100

    Refactor: refactor for deployment

--- app/valve/pdf/[id]/route.tsx
+++ app/valve/pdf/[id]/route.tsx
@@ -5,7 +5,7 @@ import ValvePDF from './ValvePDF';
 
 export async function GET(
   res: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: Promise<{ params: { id: string } }>,
 ) {
   const { id } = await params;
   const valve = await getValveProtocolOptimized(id);
