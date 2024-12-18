c8b60e9cf67416ce6264328a89c44bc782c60e2c
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-12-18 15:56:54 +0100

    Refactor: refactor SignOutBtn

--- components/common/SignOutBtn/SignOutBtn.tsx
+++ components/common/SignOutBtn/SignOutBtn.tsx
@@ -3,15 +3,22 @@
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
 import { useClerk } from '@clerk/nextjs';
 import { LogOut } from 'lucide-react';
+import toast from 'react-hot-toast';
 
 export default function SignOutBtn() {
   const { signOut } = useClerk();
+  const handleSignOut = async () => {
+    try {
+      await signOut({ redirectUrl: '/sign-in' });
+      toast.success('Wylogowano pomyślnie');
+    } catch (error) {
+      toast.error('Wystąpił błąd podczas wylogowywania');
+    }
+  };
 
   return (
-    // <SignOutButton>
-    <DropdownMenuItem onClick={() => signOut({ redirectUrl: '/sign-in' })}>
+    <DropdownMenuItem onClick={handleSignOut}>
       <LogOut /> Wyloguj
     </DropdownMenuItem>
-    // </SignOutButton>
   );
 }
